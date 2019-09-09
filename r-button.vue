d062279c4fed989435732db6bcd61247c30f29b8
ram.mart17 <alcantara.it10jbsa@gmail.com>
2019-09-09 16:44:14 +0800

    initial commit

---
+++ r-button.vue
@@ -0,0 +1,15 @@
+<template>
+	<a class="button is-primary">Primary</a>
+</template>
+
+<script>
+	import "bulma/css/bulma.css"
+	
+	export default {
+		props: {
+			colors: {
+				type: String
+			}
+		}
+	};
+</script>
